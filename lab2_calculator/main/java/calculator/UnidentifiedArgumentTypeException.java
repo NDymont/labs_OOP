20d9fc911325746f31cd95aec76fc773dd2c35b0
NDymont <91301384+NDymont@users.noreply.github.com>
2022-04-05 21:17:51 +0700

    Delete UnidentifiedArgumentTypeException.java

--- lab2_calculator/main/java/calculator/UnidentifiedArgumentTypeException.java
+++
@@ -1,8 +0,0 @@
-package calculator;
-
-public class UnidentifiedArgumentTypeException extends Exception {
-    public UnidentifiedArgumentTypeException()
-    {
-        super("unidentified argument type");
-    }
-}
